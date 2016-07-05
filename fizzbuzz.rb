116994cbc2242e7460c233abc86de1197c97ce7b
Frunez <francesco.nunez@gmail.com>
2016-07-05 08:44:03 +0100

    4th completion

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,5 @@
 def pizza_pasta
-  num = 0
-  n = num
-    while n <= 100
-      n += 1
-      puts n%15 == 0? "pizza pasta" : n%3 == 0? "pizza" : n%5 == 0? "pasta" : n
+  (1..100).each do |n| puts n%15 == 0? "pizza pasta" : n%3 == 0? "pizza" : n%5 == 0? "pasta" : n
     end
 end
 
